0dd1d8ea6f0fc7ee0e18d4ec9a228e4a6f116442
Sebastian De Ro <sebastian.dero@yahoo.de>
2017-11-17 10:55:09 +0100

    correct response type and content

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask import Response
+from flask import json
+from flask import jsonify
 app = Flask(__name__)
 
-@app.route("/data/")
-def retrieve_data():
-    return "Hello World!"
+
+@app.route("/data/", methods=['GET'])
+def data():
+    resp_data = jsonify(message="Hello, World!")
+    resp = app.make_response(resp_data)
+    resp.mimetype = "application/json"
+    return resp
